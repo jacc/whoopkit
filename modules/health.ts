b6307e5e225d0a946ff84e129ea589623ebebcbb
Jack LaFond <jack.lafond@icloud.com>
2023-10-03 22:30:30 -0400

    feat(health): get heart rate variability

--- modules/health.ts
+++ modules/health.ts
@@ -17,6 +17,7 @@ export class WhoopKitHealth {
     this.accessToken = accessToken;
   }
 
+  // TODO: type this
   async getStress(date?: string): Promise<any> {
     const request = await fetch(
       `https://api.prod.whoop.com/health-service/v2/stress-bff?timestamp=${
@@ -30,10 +31,31 @@ export class WhoopKitHealth {
     const response = await request.json();
 
     return {
-      stress: {
-        score: response.gauge.gauge_score_display,
-        text: response.gauge.gauge_subtext_display,
-      },
+      score: response.gauge.gauge_score_display,
+      text: response.gauge.gauge_subtext_display,
+      state: response.stress_state,
+      last_updated: response.last_updated_display,
+    };
+  }
+
+  async getHRV(date?: string): Promise<any> {
+    const request = await fetch(
+      `https://api.prod.whoop.com/progression-service/v3/trends/HRV?endDate=${
+        date ? date : new Date().toISOString().slice(0, 10)
+      }`,
+      {
+        headers: requestHeaders(this.accessToken),
+      }
+    );
+
+    const response = await request.json();
+
+    return {
+      week_average: response.week_time_segment.metrics[0].metric_value_display,
+      month_average:
+        response.month_time_segment.metrics[0].metric_value_display,
+      six_month_average:
+        response.six_month_time_segment.metrics[0].metric_value_display,
     };
   }
 }
